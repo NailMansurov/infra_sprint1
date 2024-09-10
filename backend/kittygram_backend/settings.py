14961e3177df852e028bedb1a10e931a2b231dc7
NailMansurov <mansurov-nf@live.com>
2024-09-10 19:37:03 +0000

    5

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_CODE')
+SECRET_KEY = os.getenv('SECRET_CODE', default='django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$')
 
 DEBUG = False
 
